c994500c958be7edb5a902592f4abf87317ffeb5
agzuniverse <aswinganesh666@gmail.com>
2018-02-21 14:03:25 +0530

    merge conflict resolved

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="test">Testing for Learning</div>
       </div>
     );
   }
