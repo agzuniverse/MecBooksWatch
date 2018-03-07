577f6acfafdee4c1f1e3fc946069b07846e1c68e
Joyal A Johney <joyalajohny@gmail.com>
2018-03-07 19:27:04 +0530

    "Landing Page done"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This site is being constructed</h1>
+        <div id="top">
+          <div id="search">
+            <input id="input" type="text" placeholder="Search for Books" />
+            <button type="submit" id="searchbutton">Begin Watch</button>
+          </div>
+          <h1 id="head">Books<span id="watch">Watch</span></h1>
+          </div>
+          <footer>
+              @Copyright Original From Model Engineering College
+          </footer>
       </div>
     );
   }
