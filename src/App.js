13b7d9ee6ef5b2d0677bb8ea994311fcdd729645
agzuniverse <aswinganesh666@gmail.com>
2018-03-07 21:48:12 +0530

    Add new book page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import SideMenu from './components/Components';
+import AddProduct from './components/AddProduct';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SideMenu/>
+        {/* <SideMenu/> */}
+        <AddProduct/>
       </div>
       
     );
