428ebea8fab564b8d97b5dee113309405f6f94bf
agzuniverse <aswinganesh666@gmail.com>
2018-03-07 22:43:12 +0530

    Integrated router

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className="App">
+            <div id="top">
+            <div id="search">
+                <input id="input" type="text" placeholder="Search for Books" />
+                <button type="submit" id="searchbutton">Begin Watch</button>
+            </div>
+            <h1 id="head">Books<span id="watch">Watch</span></h1>
+            </div>
+            <footer>
+                @Copyright Original From Model Engineering College
+            </footer>
+        </div>
+    );
+  }
+}
+
+export default Home;
