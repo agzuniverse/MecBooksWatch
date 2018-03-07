0a2ab46a455838edeb298237766598ca976d611a
agzuniverse <aswinganesh666@gmail.com>
2018-03-07 22:15:49 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import SideMenu from './components/Components';
 import AddProduct from './components/AddProduct';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* <SideMenu/> */}
-        <AddProduct/>
-      </div>
-      
+      <Router>
+        <div>
+          <Route exact path="/" component={AddProduct}/>
+          <Route path="/addproduct" component={AddProduct}/>
+          <Route path="/search" component={SideMenu}/>
+        </div>
+      </Router>      
     );
   }
 }
-
-
-
 
 export default App;
