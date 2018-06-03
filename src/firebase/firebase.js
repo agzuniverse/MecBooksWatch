de1431f11788efe0677b3e8b147ac50dd33513e3
agzuniverse <aswinganesh666@gmail.com>
2018-06-03 15:20:41 +0530

    Fixed most of the errors in firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,76 +12,88 @@ firebase.initializeApp(config);
 
 export function addToStorage(file, data) {
   return new Promise((resolve, reject) => {
-    function guid() {
-      return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+    try {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      function guid() {
+        return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+      }
+
+      const uuid = guid();
+      const storageRef = firebase.storage().ref();
+      const storageAdd = storageRef.child(uuid + file.name);
+
+      storageAdd.put(file).then(snapshot => {
+        data.imageURL = snapshot.downloadURL;
+        const database = firebase.database();
+        database
+          .ref(`store/textbooks/${data.uid}`)
+          .push(data)
+          .then(dataSnapshot => {
+            console.log(`Book added successfully dataSnapshot ${dataSnapshot}`);
+            resolve();
+          });
+      });
     }
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
+    catch(e) {
+      reject();
     }
-
-    const uuid = guid();
-
-    const storageRef = firebase.storage().ref();
-    const storageAdd = storageRef.child(uuid + file.name);
-
-    storageAdd.put(file).then(snapshot => {
-      data.imageURL = snapshot.downloadURL;
-      // console.log(data);
-
-      const database = firebase.database();
-      database
-        .ref(`store/textbooks/${data.uid}`)
-        .push(data)
-        .then(snapshot => {
-          console.log("Book added successfully");
-          resolve();
-        });
-    });
   });
 }
 
 export function readFromStorage(uid) {
   return new Promise((resolve, reject) => {
-    firebase
-      .database()
-      .ref(`store/textbooks/${uid}`)
-      .once("value", snapshot => {
-        resolve(snapshot.val());
-      });
+    try {
+      firebase
+        .database()
+        .ref(`store/textbooks/${uid}`)
+        .once("value", snapshot => {
+          resolve(snapshot.val());
+        });
+    }
+    catch(e) {
+      reject();
+    }
   });
 }
 
 export function searchAll(query) {
   query = query.toLowerCase();
   return new Promise((resolve, reject) => {
-    firebase
-      .database()
-      .ref("store/textbooks/")
-      .once("value", snapshot => {
-        const data = snapshot.val();
-        const results = [];
-        let flag = true;
-        for (const i in data) {
-          for (const j in data[i]) {
-            for (const tag in data[i][j].tags) {
-              if (data[i][j].tags[tag].toLowerCase().includes(query)) {
-                flag = true;
-                for (const k in results) {
-                  if (results[k] === data[i][j]) {
-                    flag = false;
+    try {
+      firebase
+        .database()
+        .ref("store/textbooks/")
+        .once("value", snapshot => {
+          const data = snapshot.val();
+          const results = [];
+          let flag = true;
+          for (const i in data) {
+            for (const j in data[i]) {
+              for (const tag in data[i][j].tags) {
+                if (data[i][j].tags[tag].toLowerCase().includes(query)) {
+                  flag = true;
+                  for (const k in results) {
+                    if (results[k] === data[i][j]) {
+                      flag = false;
+                    }
                   }
-                }
-                if (flag) {
-                  results.push(data[i][j]);
+                  if (flag) {
+                    results.push(data[i][j]);
+                  }
                 }
               }
             }
           }
-        }
-        resolve(results);
-      });
+          resolve(results);
+        });
+    }
+    catch(e) {
+      reject();
+    }
   });
 }
 
