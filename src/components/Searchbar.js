51c2d9404b16b5deb777670a9282777d0773d37e
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-06-10 19:03:56 +0530

    fixed eslint error

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,27 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../App.css";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import IconButton from "material-ui/IconButton";
+import NavigationMenu from "material-ui/svg-icons/navigation/menu";
 
-const Searchbar = (props) => {
-    return(
-      <div className="Searchbar">
-        <MuiThemeProvider>
-          <div id="iconDiv">
-            <IconButton>
-              <NavigationMenu />
-            </IconButton>
-          </div>
-          <div id="inputDiv">
-            <form onSubmit={props.search} id="searchForm">
-              <input id="input2" type="text" placeholder="Search for Books" />
-            </form>
-          </div>
-        </MuiThemeProvider>
+const Searchbar = props => (
+  <div className="Searchbar">
+    <MuiThemeProvider>
+      <div id="iconDiv">
+        <IconButton>
+          <NavigationMenu />
+        </IconButton>
       </div>
-    )
-}
+      <div id="inputDiv">
+        <form onSubmit={props.search} id="searchForm">
+          <input id="input2" type="text" placeholder="Search for Books" />
+        </form>
+      </div>
+    </MuiThemeProvider>
+  </div>
+  );
 
 Searchbar.propTypes = {
   search: PropTypes.func.isRequired
