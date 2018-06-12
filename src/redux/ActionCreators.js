55a5253fc0f15a0800d040375f19645ef44aa799
agzuniverse <aswinganesh666@gmail.com>
2018-06-12 22:58:24 +0530

    Use arrow functions over regular functions

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,55 +1,55 @@
 import { TEST, SET_UID, SET_NAME, SET_EMAIL, SET_PROPIC, SEARCH_STRING, SEMFILTER, BRANCHFILTER } from './Reducers';
 
-export function testRedux(text){
+export const testRedux = (text) => {
     return {
         type: TEST,
         text
     }
 }
 
-export function setGlobalUid(uid){
+export const setGlobalUid = (uid) => {
     return {
         type: SET_UID,
         uid
     }
 }
 
-export function setGlobalName(name){
+export const setGlobalName = (name) => {
     return {
         type: SET_NAME,
         name
     }
 }
 
-export function setGlobalEmail(email){
+export const setGlobalEmail = (email) => {
     return {
         type: SET_EMAIL,
         email
     }
 }
 
-export function setGlobalProPic(propic){
+export const setGlobalProPic = (propic) => {
     return {
         type: SET_UID,
         propic
     }
 }
 
-export function searchString(query){
+export const searchString = (query) => {
     return {
         type: SET_UID,
         query
     }
 }
 
-export function setSemFilter(sem){
+export const setSemFilter = (sem) => {
     return {
         type: SET_UID,
         sem
     }
 }
 
-export function setBranchFilter(branch){
+export const setBranchFilter = (branch) => {
     return {
         type: SET_UID,
         branch
