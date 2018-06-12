b6b535171bd4162ffd81411edee853fb19ff4ea2
agzuniverse <aswinganesh666@gmail.com>
2018-06-12 23:01:21 +0530

    Standardised reducer

--- src/redux/Reducers.js
+++ src/redux/Reducers.js
@@ -1,33 +1,27 @@
-const reducers = (state, action) => {
-    if (action.type === "TEST") {
-        return Object.assign({}, state, { test: action.payload.test });
-    } else if (action.type === "SET_UID") {
-        return Object.assign({}, state, { uid: action.payload.uid });
-    } else if (action.type === "SET_NAME") {
-        return Object.assign({}, state, { name: action.payload.name });
-    } else if (action.type === "SET_EMAIL") {
-        return Object.assign({}, state, { email: action.payload.email });
-    } else if (action.type === "SET_PROPIC") {
-        return Object.assign({}, state, { proPic: action.payload.propic });
-    } else if (action.type === "SEARCH_STRING") {
-        return Object.assign({}, state, { query: action.payload.query });
-    } else if (action.type === "SEMFILTER") {
-        return Object.assign({}, state, { semFilter: action.payload.sem });
-    } else if (action.type === "BRANCHFILTER") {
-        return Object.assign({}, state, { branchFilter: action.payload.branch });
+import { TEST, SET_UID, SET_NAME, SET_EMAIL, SET_PROPIC, SEARCH_STRING, SEMFILTER, BRANCHFILTER } from './Reducers';
+import InitialState from './InitialState';
+
+const reducers = (state = InitialState, action) => {
+    switch (action.type) {
+        case TEST :
+            return Object.assign({}, state, { test: action.test });
+        case SET_UID:
+            return Object.assign({}, state, { uid: action.uid });
+        case SET_NAME:
+            return Object.assign({}, state, { name: action.name });
+        case SET_EMAIL:
+            return Object.assign({}, state, { email: action.email });
+        case SET_PROPIC:
+            return Object.assign({}, state, { proPic: action.propic });
+        case SEARCH_STRING:
+            return Object.assign({}, state, { query: action.query });
+        case SEMFILTER:
+            return Object.assign({}, state, { semFilter: action.sem });
+        case BRANCHFILTER:
+            return Object.assign({}, state, { branchFilter: action.branch });
+        default:
+            return state;
     }
-    return state;
 };
 
 export default reducers;
-
-// {
-//   test: "incomplete",
-//   uid: "",
-//   name: "",
-//   proPic: "",
-//   email: "",
-//   query: "",
-//   semFilter: "Any semester",
-//   branchFilter: "Any branch"
-// });
