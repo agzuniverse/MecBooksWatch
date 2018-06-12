f95eee710bf15c95d83b7a6ddb25405e94dc6583
agzuniverse <aswinganesh666@gmail.com>
2018-06-12 21:18:21 +0530

    Defined actions in Actions.js

---
+++ src/redux/Actions.js
@@ -0,0 +1,8 @@
+export const TEST = 'TEST';
+export const SET_UID = 'SET_UID';
+export const SET_NAME = 'SET_NAME';
+export const SET_EMAIL = 'SET_EMAIL';
+export const SET_PROPIC = 'SET_PROPIC';
+export const SEARCH_STRING = 'SEARCH_STRING';
+export const SEMFILTER = 'SEMFILTER';
+export const BRANCHFILTER = 'BRANCHFILTER';
