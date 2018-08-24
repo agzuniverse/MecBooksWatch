b9ea69de32bee357998d0f996795cff369a18b62
Aswin G <aswinganesh666@gmail.com>
2018-08-24 22:32:19 +0530

    Made build passing

--- src/components/auth.js
+++ src/components/auth.js
@@ -145,7 +145,4 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
