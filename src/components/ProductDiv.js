ff4bbaf7b4ceb55f295f88605d940b6b7d57650d
Aswin G <aswinganesh666@gmail.com>
2018-09-09 13:10:00 +0530

    Removed unused assets and imports

--- src/components/ProductDiv.js
+++ src/components/ProductDiv.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "../App.css";
-import "../assets/book1.jpg";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 const ProductDiv = ({ details }) => {
