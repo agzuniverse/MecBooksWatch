5106fe1acc526e10e36b99f9f090028d67df838c
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-09-23 20:23:29 +0530

    move to bleve search

--- search-indexing-server/main.go
+++ search-indexing-server/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	firebase "firebase.google.com/go"
-	"github.com/algolia/algoliasearch-client-go/algoliasearch"
-	"github.com/fatih/structs"
+	"github.com/blevesearch/bleve"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"google.golang.org/api/option"
@@ -32,66 +31,160 @@ type Textbook struct {
 	FileID    string `json:"fileID" firestore:"fileID"`
 }
 
-type ReqData struct {
-	IDToken    string   `json:"idToken"`
-	TextbookID string   `json:"textbookID"`
-	Data       Textbook `json:"data"`
+type PostReqData struct {
+	IDToken string   `json:"idToken"`
+	Data    Textbook `json:"data"`
+}
+
+type DelReqData struct {
+	IDToken string `json:"idToken"`
+	ID      string `json:"bookID"`
+}
+
+type SearchReqData struct {
+	Query string `json:"query"`
 }
 
 func main() {
-	client := algoliasearch.NewClient(os.Getenv("APPID"), os.Getenv("APIKEY"))
-	index := client.InitIndex("textbooks")
+	mapping := bleve.NewIndexMapping()
+	index, err := bleve.New("textbooks.bleve", mapping)
+	if err != nil {
+		log.Fatal(err)
+	}
 	opt := option.WithCredentialsFile("./serviceaccount.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	r.HandleFunc("/postbook", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
-		var reqdata ReqData
-		var storedata Textbook
+		var reqdata PostReqData
 		b, _ := ioutil.ReadAll(r.Body)
 		if err := json.Unmarshal(b, &reqdata); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
+			return
 		}
+		fmt.Println(reqdata)
 		client, err := app.Auth(context.Background())
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("error getting Auth client: " + err.Error()))
+			return
 		}
 		idToken := reqdata.IDToken
 		_, errr := client.VerifyIDToken(context.Background(), idToken)
 		if errr != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("error verifying ID token: " + err.Error()))
+			w.Write([]byte("error verifying ID token: " + errr.Error()))
+			return
 		}
-		textbookID := reqdata.TextbookID
 		firestore, err := app.Firestore(context.Background())
+		ref, _, err := firestore.Collection("textbooks").Add(context.Background(), reqdata.Data)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
+			return
+		} else {
+			textbookID := ref.ID
+			index.Index(textbookID, reqdata.Data)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Textbook added successfully"))
 		}
 		defer firestore.Close()
-		snap, err := firestore.Collection("textbooks").Doc(textbookID).Get(context.Background())
+	}).Methods("POST")
+
+	r.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var reqdata SearchReqData
+		b, _ := ioutil.ReadAll(r.Body)
+		if err := json.Unmarshal(b, &reqdata); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		query := bleve.NewFuzzyQuery(reqdata.Query)
+		req := bleve.NewSearchRequest(query)
+		result, err := index.Search(req)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
+			return
 		}
-		snap.DataTo(&storedata)
-		if storedata == reqdata.Data {
-			o := []algoliasearch.Object{
-				structs.Map(storedata),
+		var resultTextbooks []Textbook
+		for _, v := range result.Hits {
+			firestore, err := app.Firestore(context.Background())
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
 			}
-			index.AddObjects(o)
-			w.Write([]byte("Book indexed"))
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Data invalid"))
+			ch := make(chan Textbook)
+			go func() {
+				docsnap := firestore.Collection("textbooks").Doc(v.ID)
+				datasnap, err := docsnap.Get(context.Background())
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(err.Error()))
+					return
+				}
+				var textbook Textbook
+				if err := datasnap.DataTo(&textbook); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(err.Error()))
+					return
+				}
+				ch <- textbook
+
+			}()
+			resultTextbooks = append(resultTextbooks, <-ch)
 		}
+		w.WriteHeader(http.StatusOK)
+		jsonBody, _ := json.Marshal(resultTextbooks)
+		w.Write([]byte(jsonBody))
+	}).Methods("GET")
+
+	r.HandleFunc("/deletebook", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		var reqdata DelReqData
+		b, _ := ioutil.ReadAll(r.Body)
+		if err := json.Unmarshal(b, &reqdata); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		fmt.Println(reqdata)
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("error getting Auth client: " + err.Error()))
+			return
+		}
+		idToken := reqdata.IDToken
+		_, errr := client.VerifyIDToken(context.Background(), idToken)
+		if errr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("error verifying ID token: " + errr.Error()))
+			return
+		}
+		firestore, err := app.Firestore(context.Background())
+		_, err = firestore.Collection("textbooks").Doc(reqdata.ID).Delete(context.Background())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		} else {
+			index.Delete(reqdata.ID)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Textbook Deleted successfully"))
+		}
+		defer firestore.Close()
+
 	}).Methods("POST")
+
 	corshandler := cors.Default().Handler(r)
 	http.Handle("/", corshandler)
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+	log.Fatal(http.ListenAndServe(":4000", nil))
 }
